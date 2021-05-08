e28f545a63fe9597e4a7d925b0c24fa6c597a792
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-05-08 12:52:37 -0600

    update code

---
+++ src/App.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="container">
+    <Header title="Task List" />
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      v-bind:tasks="tasks"
+    />
+    <p>*double click to set/remove a reminder*</p>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header';
+import Tasks from './components/Tasks';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Tasks,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    deleteTask(id) {
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => task.id !== id);
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: 'Test1',
+        day: 'March 1st at 2:30 PM',
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: 'Test2',
+        day: 'March 5th at 5:30 PM',
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: 'Test3',
+        day: 'March 10th at 10:30 PM',
+        reminder: false,
+      },
+    ];
+  },
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+  user-select: none;
+}
+
+.container p {
+  font-size: 12px;
+}
+
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
